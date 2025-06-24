41c030bb6dc208155b4f66a51cceb77cc2a46466
Sakshi Chaturvedi <sakshi3012002@gmail.com>
2025-06-24 20:36:45 +0530

    fix: setup server.js for Render

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-const jsonServer = require('json-server');
+// server.js
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+
+const PORT = process.env.PORT || 8080;
 
 server.use(middlewares);
 server.use(router);
-const PORT = process.env.PORT || 8080;
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running on port ${PORT}`);
